d11e16f4ca236d4fb685be3e5d0cf6d57d610c69
Tenka5 <vteremetskiy@gmail.com>
2024-03-22 18:26:06 +0300

    modified:   src/components/Navbar.vue

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -2,7 +2,7 @@
   <v-app-bar :elevation="10" color="orange-lighten-2">
     <v-app-bar-title><router-link class="link" to="/"><h1>Магазин книг</h1></router-link></v-app-bar-title>
       <template v-slot:append>
-        <v-btn variant="tonal" class="ml-5"><router-link class="link" to="/Drawer">Корзина</router-link></v-btn>
+        <v-btn variant="tonal" class="ml-5"><router-link class="link" to="/Drawer">Корзина1</router-link></v-btn>
         <v-btn variant="tonal" class="ml-5"><router-link class="link" to="/Editor">Редактировать товар</router-link></v-btn>
       </template>
   </v-app-bar>
